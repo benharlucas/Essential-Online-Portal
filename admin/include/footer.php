9560f41d4c07916e03acc4bd1167bc39eab6b615
unknown <benhar.masahod@msugensan.edu.ph>
2022-01-24 20:08:45 +0800

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2022 Essential Shopping Portal by Essential Shop </b> All rights reserved.
+		</div>
+	</div>
